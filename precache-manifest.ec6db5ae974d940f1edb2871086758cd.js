bd454b7bf601b42da48511a89c7ac7e7f1f2f7a2
Dallas Read <dallas@excitecreative.ca>
2022-04-17 20:47:18 -0300

    gh-pages

---
+++ precache-manifest.ec6db5ae974d940f1edb2871086758cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f852392eb2ec7ce320",
+    "url": "/wrangle/css/app.9ff5fe75.css"
+  },
+  {
+    "revision": "d4ba38cefe0c4ef4e706b9c4d5178e30",
+    "url": "/wrangle/index.html"
+  },
+  {
+    "revision": "78f852392eb2ec7ce320",
+    "url": "/wrangle/js/app.d745d6fb.js"
+  },
+  {
+    "revision": "943c1076b7c4e404633b",
+    "url": "/wrangle/js/chunk-vendors.38deb75a.js"
+  },
+  {
+    "revision": "69cb8b8b9bd9c712588904be463be05e",
+    "url": "/wrangle/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wrangle/robots.txt"
+  }
+]);
